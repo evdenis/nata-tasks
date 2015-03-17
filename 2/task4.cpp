d6b8f423007807d1c1fd6e501412881909d8b111
Denis Efremov <yefremov.denis@gmail.com>
2015-03-17 23:44:13 +0300

    week 2: task 4

---
+++ 2/task4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a, b, c;
+   int sum = 0;
+
+   for(a = 1, b = 2, c = 3; a <= 100; ++a, ++b, ++c) {
+      int t = b - c; // всегда -1
+      sum += a + t;
+      //cout << a << ": " << sum << endl;
+   }
+   cout << sum << endl;
+
+   return 0;
+}
