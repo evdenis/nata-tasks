b50ebc7122e9f78b4a4d97a52a0c7068ff136617
Denis Efremov <yefremov.denis@gmail.com>
2015-03-17 23:05:37 +0300

    week2: task2

---
+++ 2/task2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   unsigned int a = 0, b = 1;
+   unsigned int limit = 47; // Больше - происходит целочисленное переполнение переменной fib
+
+   cout << "0: 0" << endl;
+   cout << "1: 1" << endl;
+   for(unsigned int n = 2; n <= limit; ++n)
+   {
+      unsigned int fib = a + b;
+
+      cout << n << ": " << fib << endl;
+
+      a = b;
+      b = fib;
+   }
+
+   return 0;
+}
