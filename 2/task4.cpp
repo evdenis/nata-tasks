c1fc9b9fd7e44a17fc5e82c780d6c718c4524fc2
Denis Efremov <yefremov.denis@gmail.com>
2015-03-18 11:05:13 +0300

    week 2: task 4: fix

--- 2/task4.cpp
+++ 2/task4.cpp
@@ -6,8 +6,8 @@ int main()
 {
    int a, b, c;
    int sum = 0;
-
-   for(a = 1, b = 2, c = 3; a <= 100; ++a, ++b, ++c) {
+                                    // ++a, ++b, ++c
+   for(a = 1, b = 2, c = 3; a <= 100; a = b, b = c, ++c) {
       int t = b - c; // всегда -1
       sum += a + t;
       //cout << a << ": " << sum << endl;
