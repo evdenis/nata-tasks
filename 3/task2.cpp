43567936286c0e107e3eb4f950d2590406113c7e
Denis Efremov <yefremov.denis@gmail.com>
2015-04-07 15:57:43 +0300

    week 3: task 2 fix

--- 3/task2.cpp
+++ 3/task2.cpp
@@ -2,17 +2,28 @@
 
 using namespace std;
 
-#define ARRAY_SIZE 100
-
 int main()
 {
-   int array[ARRAY_SIZE];
+   int array[10];
+   
+   cout << "Array:  ";
+   for(int i = 0; i < 10; ++i) { // заполняем цифрами от 1 до 10
+      array[i] = i + 1;
+      cout << array[i] << " ";
+   }
+   cout << endl;
 
-   for(int i = 0; i < ARRAY_SIZE; i = i + 2) {
+   for(int i = 0; i < 10; i = i + 2) { // меняем местами попарно
       int tmp      = array[i];
       array[i]     = array[i + 1];
       array[i + 1] = tmp;
    }
+   
+   cout << "Result: ";
+   for(int i = 0; i < 10; ++i) { // вывод результата
+      cout << array[i] << " ";
+   }
+   cout << endl;
 
    return 0;
 }
